d0393c6dcdb124368ed3033fe1eb9e1385f70800
7ussain Mohamed <hlhols313@gmail.com>
2023-03-30 15:38:58 +0300

    adding project to main branch

--- dowloder.py
+++ dowloder.py
@@ -1,9 +1,10 @@
 # import youtube from pytube laibrary
 from pytube import YouTube
-
+#declare var that include 
 link = (input("enter your url: "))
 yt = YouTube(link);
 
+#this code download the vedio in resloution 480
 stream = yt.streams.get_by_resolution(480).download();
 
 print("download done")
