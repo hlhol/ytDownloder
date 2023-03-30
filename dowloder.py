e2060bac611f4b2c59897f283a74bd635a4dd2c6
7ussain Mohamed <hlhols313@gmail.com>
2023-03-30 16:05:44 +0300

    adding a spacfic location to save vedio in it

--- dowloder.py
+++ dowloder.py
@@ -1,10 +1,22 @@
 # import youtube from pytube laibrary
-from pytube import YouTube
+from pytube import YouTube;
+
+#import os
+import os;
+
 #declare var that include 
 link = (input("enter your url: "))
 yt = YouTube(link);
 
-#this code download the vedio in resloution 480
-stream = yt.streams.get_by_resolution(480).download();
 
-print("download done")
+#add spacfic location for folder and the r before the path to the program understand the \ backslash.
+folder = r"add your path for the folder that you want to save vedio in"
+
+#this code download the vedio in resloution 480.
+# you can add spacific folder to save file in.
+stream = yt.streams.get_by_resolution(480).download(folder);  
+
+
+
+
+print("download done");
