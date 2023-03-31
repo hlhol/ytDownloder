5a8f387ac6cffa6339b2aac5cf1e4e4add475922
7ussain Mohamed <hlhols313@gmail.com>
2023-03-31 16:56:15 +0300

    l'm modfiye the project from one vedio to playlist

--- dowloder.py
+++ dowloder.py
@@ -1,22 +1,23 @@
-# import youtube from pytube laibrary
-from pytube import YouTube;
+import pytube;
 
-#import os
-import os;
+#you can edit the path of folder, but don't edit the r before path.
+folder = r"write your folder path ";
 
-#declare var that include 
-link = (input("enter your url: "))
-yt = YouTube(link);
+# Set the URL of the playlist to download by input
+playlist_url = input("enter url: ");
 
+# Create a Playlist object using the pytube library
+playlist = pytube.Playlist(playlist_url);
 
-#add spacfic location for folder and the r before the path to the program understand the \ backslash.
-folder = r"add your path for the folder that you want to save vedio in"
+# Loop through each video in the playlist and download it
+for video_url in playlist.video_urls:
+    # Create a YouTube object for the video
+    youtube = pytube.YouTube(video_url);
 
-#this code download the vedio in resloution 480.
-# you can add spacific folder to save file in.
-stream = yt.streams.get_by_resolution(480).download(folder);  
+    # Select the 480 resolution stream
+    stream = youtube.streams.get_by_resolution(480);
 
-
-
-
-print("download done");
+    # Download the stream
+    print(f"Downloading {youtube.title}...");  #youtube.title is to get vedio title and print it.
+    stream.download(folder); #add spacific location to downloded vedio.
+    print(f"{youtube.title} downloaded successfully!\n") #print dwnload 
