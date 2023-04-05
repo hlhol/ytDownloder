eefef8dedbc204ca4391b385e10e2f71a41a1b81
7ussain Mohamed <hlhols313@gmail.com>
2023-04-05 15:44:59 +0300

    l edit the code and now you can choise and the code work

--- dowloder.py
+++ dowloder.py
@@ -1,7 +1,7 @@
 import pytube
 
 #You can edit the path of folder, but don't edit the r before path.
-folder = r"write your folder path "
+folder = r"you can put a path of folder that vedio will be in."
 
 #Ask user to choose what to download
 choice = input("Enter '1' to download a single video or '2' to download a playlist: ")
@@ -12,21 +12,21 @@ if choice == '1':
     yt = pytube.YouTube(link)
     stream = yt.streams.get_by_resolution(480)
     print(f"Downloading {yt.title}...")
-    downloaded_file = stream.download()
+    downloaded_file = stream.download(folder)
     if downloaded_file:
-    print(f"{yt.title} downloaded successfully!\n")
+        print(f"{yt.title} downloaded successfully!\n")
     else:
         print(f"Failed to download {yt.title}\n")
 
 #If choice is 2, download a playlist
 elif choice == '2':
-playlist_url = input("Enter playlist URL: ")
-playlist = pytube.Playlist(playlist_url)
-for video_url in playlist.video_urls:
-    youtube = pytube.YouTube(video_url)
-    stream = youtube.streams.get_highest_resolution()
-    print(f"Downloading {youtube.title}...")
-    downloaded_file = stream.download(folder)
+    playlist_url = input("Enter playlist URL: ")
+    playlist = pytube.Playlist(playlist_url)
+    for video_url in playlist.video_urls:
+        youtube = pytube.YouTube(video_url)
+        stream = youtube.streams.get_highest_resolution()
+        print(f"Downloading {youtube.title}...")
+        downloaded_file = stream.download(folder)
     if downloaded_file:
         print(f"{youtube.title} downloaded successfully!\n")
     else:
